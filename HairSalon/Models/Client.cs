f9b8fe28864c2af306d000dba635a5140d731219
ryandefea <ryan.defea@gmail.com>
2022-07-29 16:24:11 -0700

    finally have phone number working, had a typo that i could not find for a long time.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,6 +6,8 @@ namespace HairSalon.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int StylistId { get; set; }
+
+    public string PhoneNumber { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
 }
